39062ef3c08be60eea2c72b816f77cb78063ec11
usama-hameed <musamahameed97@gmail.com>
2024-02-29 03:16:34 +0100

    total number of words in file calculated

--- ccwc.py
+++ ccwc.py
@@ -6,13 +6,26 @@ def get_bytes(file):
     return file.stat().st_size
 
 
-OPTIONS = {'c': get_bytes}
+def get_line(file):
+    print(type(file))
+    with open(file, 'r', encoding="utf8") as f:
+        return len(f.readlines())
+
+
+def get_words(file):
+    total_words = 0
+    with open(file, 'r', encoding="utf8") as f:
+        for words in f.readlines():
+            total_words += len(words.split())
+    return total_words
 
 
 parser = argparse.ArgumentParser(description='wc tool')
 
 parser.add_argument("path")
 parser.add_argument('-c', action='store_true', help='Number of Bytes in File')
+parser.add_argument('-l', action='store_true', help='Number of Lines in File')
+parser.add_argument('-w', action='store_true', help='Number of Words in File')
 
 args = parser.parse_args()
 
@@ -20,3 +33,9 @@ text_file = Path(args.path)
 
 if args.c:
     print(get_bytes(text_file), text_file)
+
+elif args.l:
+    print(get_line(text_file), text_file)
+
+elif args.w:
+    print(get_words(text_file), text_file)
